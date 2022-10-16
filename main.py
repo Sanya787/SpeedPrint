db6ecee4166571ee575882805799105dba09d44e
Sanya787 <114693100+Sanya787@users.noreply.github.com>
2022-10-16 14:16:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,26 @@
 import sys
 
+from extra import Window
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel
 
 
 class App(QWidget):
-
     def __init__(self):
         super().__init__()
+        self.click_button = 0
         self.initUI()
 
     def load_interface(self):
         uic.loadUi('project.ui', self)
-
-    def append_widget(self, mark):
-        '''Displays a pop-up window with the result of the test'''
-
-        if mark == 'good':
-            pass
-        else:
-            pass
+        self.button_start = self.findChild(QPushButton, 'pushButton_62')
+        self.button_start.clicked.connect(self.start_button_func)
+        self.timer = self.findChild(QLabel, 'label_10')
 
     def start_button_func(self):
-        '''This method is bound to the "Начать" button.
-         Gets the text from the database and passes it to the test_moment method'''
-        some_text = 'lallala'
-
-        self.append_widget(self.test_moment(some_text))
-
-    def test_moment(self, some_text):
-        '''Returns a good/bad result which is then displayed on the screen.
-         Tracks keyboard input, counts time, errors and typing speed'''
-
         pass
 
     def initUI(self):
-        '''This main func in class App. This method is the binder of all other methods.
-        Returns nothing'''
-
         self.load_interface()
 
 
@@ -45,5 +28,9 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = App()
     window.show()
+
+    app2 = QApplication(sys.argv)
+    window2 = Window()
+    window2.show()
+    sys.exit(app2.exec())
     sys.exit(app.exec())
-    
