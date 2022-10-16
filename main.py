4c6c9d1479bb27b16471cba1d017563e50aa7eba
Sanya787 <114693100+Sanya787@users.noreply.github.com>
2022-10-16 14:47:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 
 from extra import Window
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QTextBrowser
 
 
 class App(QWidget):
@@ -11,16 +11,33 @@ class App(QWidget):
         self.click_button = 0
         self.initUI()
 
+    def append_widget(self, mark):
+        '''Displays a pop-up window with the result of the test'''
+
+        if mark == 'good':
+            pass
+        else:
+            pass
+
     def load_interface(self):
+        '''This method loads the entire interface into the widget
+        Returns nothing'''
+
         uic.loadUi('project.ui', self)
         self.button_start = self.findChild(QPushButton, 'pushButton_62')
         self.button_start.clicked.connect(self.start_button_func)
         self.timer = self.findChild(QLabel, 'label_10')
+        self.browser = self.findChild(QTextBrowser, 'textBrowser')
 
     def start_button_func(self):
+        '''This method is bound to the "Начать" button.
+         Gets the text from the database and passes it to the test_moment method'''
+        self.browser.setText("Какой-то текст из базы данных")
         pass
 
     def initUI(self):
+        '''This main func in class App. This method is the binder of all other methods.
+        Returns nothing'''
         self.load_interface()
 
 
